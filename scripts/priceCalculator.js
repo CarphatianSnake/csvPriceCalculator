8fdbb8cee969057eea479f13631e661e6fedef5e
Dmytro <sunsn4ke@gmail.com>
2021-11-10 19:57:43 +0200

    заменил в коде файл доя подсчета на 24-rexel.json

--- scripts/priceCalculator.js
+++ scripts/priceCalculator.js
@@ -17,7 +17,7 @@ priceForm.onsubmit = async (e) => {
     seriesDecreaseValue: Number(inputValues.unifMod.value) / 100
   };
   
-  let jsonRequest = new Request('data/22-bulros.json');
+  let jsonRequest = new Request('data/24-rexel.json');
   fetch(jsonRequest)
   .then(function(response) {
       return response.json();
